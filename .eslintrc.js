3c2f66c1f1c2530d2a28839dbfc8731694dba9b5
sheldar <kewin.polok@gmail.com>
2021-01-30 18:07:34 +0100

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ['standard', 'prettier', 'prettier/standard'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
